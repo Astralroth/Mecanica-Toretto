ec89d40a184a2dbbb785164225f8bad6e11ffd2a
Astralroth <francosandoval003@gmail.com>
2023-05-09 13:31:53 -0400

    Cambios de DataSets
    cambios de dataset

--- pedido/models.py
+++ pedido/models.py
@@ -18,3 +18,6 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
