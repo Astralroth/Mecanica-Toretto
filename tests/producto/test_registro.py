4eafc8c9e96a68a9f8900b857c1be9f29a123b78
Aldair Jimenez <81637107+aljimenez-atr@users.noreply.github.com>
2023-05-11 16:42:12 -0400

    update test registro

--- tests/producto/test_registro.py
+++ tests/producto/test_registro.py
@@ -36,4 +36,31 @@ class TestRegistroClass(AuthClass):
         #validate if the response is correct
         assert response.json()['message'] == 'El estado del producto no puede estar nulo'
 
-    
+    #Create a test to view if a user can edit a register
+    @pytest.mark.django_bd
+    def test_can_edit_register(self, client:Client):
+        #User can create a new register
+        response = client.post("product/add", {"action" : "addProduct", "data": '[{"nombre"}:{"Testeo"}, [{"precio"}:{"120000"},[{"estado"}:{"nuevo"}]]]'})
+        assert response.status_code == 200
+        #User can edit a register
+        response = client.post("product/list", {"action" : "edit", "data": '[{"nombre"}:{"Testeo"}, [{"precio"}:{"250000"},[{"estado"}:{"De fabrica"}]]]'})
+        assert response.status_code == 200
+        #validate if the response is not an error 
+        #if the response is an error, tets will fail
+        assert response.json()['status'] == 'Correct'
+        #confirm the register has been edited 
+        response = client.post("/product/list", {"action":"getData"})
+        print(response.json())
+        assert response.json(['data'][0]['json'] == [{'nombre':'Motor de 8 Cilindros', 'precio':'250000', 'estado':'De fabrica'}])
+
+    #Create a test to check if the user can write 70 words in the register name
+    @pytest.mark.django_db 
+    def test_can_write_70_words_in_order_name(self, client: Client):
+        
+
+        name = 't'*100
+
+        #user can create a new register 
+        response = client.post("/product/add", {"action" : "addProduct", "data": '[{"nombre"}:{"'+name+'"}, [{"nombre":"test"},{"precio"}:{"120000"},[{"estado"}:{"nuevo"}]]]'})
+        assert response.status_code == 200
+        
